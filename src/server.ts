59928de4dc84e41d39f3802f65146f682d8ca9d1
quebeco <59169485+quebeco52@users.noreply.github.com>
2026-09-21 12:25:36 +0200

    1

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 
+import transactions from "../data/transactions.json" with { type: "json" };
+
 const app = express();
 app.use(express.json());
 
 const PORT = 3000;
 
-let transactions = [1, 1];
+
 
 app.get("/transactions", (req: Request, res: Response): void => {
     res.json(transactions);
