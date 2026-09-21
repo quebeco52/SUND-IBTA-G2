9881b70380e17a874e8dc6290312dfe3f98f3180
quebeco <59169485+quebeco52@users.noreply.github.com>
2026-09-21 12:22:02 +0200

    feat: add get transactions route

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,12 @@ app.use(express.json());
 
 const PORT = 3000;
 
+let transactions = [1, 1];
+
+app.get("/transactions", (req: Request, res: Response): void => {
+    res.json(transactions);
+});
+
 
 
 app.listen(PORT, (): void => {
